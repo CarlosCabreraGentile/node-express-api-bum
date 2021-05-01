271c445dc215f2499282110ea515fd3230df864d
Carlos Cabrera Gentile <carloscgentile@gmail.com>
2021-05-01 20:44:38 -0300

    xss protection and security headers

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const DBConnection = require('./config/dbConnection');
 const colors = require('colors');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 const errorHandler = require('./middleware/error'); 
 const fileupload = require('express-fileupload');
 //const logger = require('./middleware/logger'); //example logger middleware
@@ -44,6 +46,12 @@ app.use(fileupload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attack cross site scripting
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
